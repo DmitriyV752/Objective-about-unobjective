26c5b69fb7e10b944528611e81c4b51403d2e082
DmitriyV752 <d.medved236@gmail.com>
2025-06-30 13:24:48 +0000

    3.3

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,23 @@ document.addEventListener("DOMContentLoaded", () => {
     // *        3.3.2. Нет: продолжаем.
     // * 4. Конец
     console.log('Скрипт отработал корректно')
+
+    // Объявляем переменную-массив intensiveImg и сохраняем в нее все элементы на странице с классом intensive__img  
+    const intensiveImg = document.querySelectorAll('.prouseful__item__img');
+
+    // Пройдемся по каждому элементу массива intensiveImg, с помощью цикла forEach.  Внутри функции 2 переменные: 
+    // item - текущее изображение, а index — его номер в массиве, начиная с 0
+    intensiveImg.forEach((item, index) => {
+    // Объявляем переменную intensiveText и сохраняем в нее все элементы с классом intensive__description, которые связаны с описаниями для изображений
+        const intensiveText = document.querySelectorAll('.prouseful__item__description');
+    // Когда курсор наводится на изображение (событие mouseenter), срабатывает обработчик события mouseenter:
+        item.addEventListener('mouseenter', () => {
+        intensiveText[index].removeAttribute('hidden');
+    });
+    // Когда курсор убираем с изображения (событие mouseleave), срабатывает обработчик события mouseleave:
+        item.addEventListener('mouseleave', () => {
+        intensiveText[index].setAttribute('hidden', true);
+        });
+    });
+
 });
